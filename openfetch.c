7eb33a99a8791e660cba4ad5d22f47cf849dce39
David Uhden <david@uhden.dev>
2025-02-24 22:59:22 +0100

    C11

--- openfetch.c
+++ openfetch.c
@@ -15,6 +15,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <sys/utsname.h>
